d8a4f73dad8eadc5726bb6349b042cfb66252518
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-09-22 13:48:43 +0200

    variables support

--- examples/example.js
+++ examples/example.js
@@ -5,6 +5,6 @@ piREST.set_id('34f5eQ');
 piREST.set_name('my_new_Pi');
 
 temperature = 24;
-humidity = 24;
+humidity = 40;
 piREST.variable('temperature',temperature);
 piREST.variable('humidity',humidity);
