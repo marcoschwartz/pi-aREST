21ce5be6da6bd2d9d48d14a7c586f09b6d310c93
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-09-25 08:28:15 +0200

    updated example

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,9 @@
 // Start
-var piREST = require('./../index.js');
+var express = require('express');
+var piREST = require('./../index.js')(app);
+
+// Express app
+var app = express();
 
 piREST.set_id('34f5eQ');
 piREST.set_name('my_new_Pi');
@@ -9,8 +13,12 @@ humidity = 40;
 piREST.variable('temperature',temperature);
 piREST.variable('humidity',humidity);
 
-piREST.app.get('/', function(req, res){
+app.get('/', function(req, res){
   
   res.send("Welcome to the home page");
 
+});
+
+var server = app.listen(80, function() {
+    console.log('Listening on port %d', server.address().port);
 });
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Require
-var express = require('express');
 var gpio = require("pi-gpio");
 var RaspiCam = require("raspicam");
 
@@ -13,12 +12,6 @@ var camera = new RaspiCam({mode: "photo",
   n: true
 });
 
-// Create app
-var app = express();
-
-// Expose pictures dir
-app.use(express.static('./public'));
-
 // Pi aREST class
 var pi = {
   id: '001',
@@ -31,94 +24,94 @@ var variables = new Object();
 variables['temperature'] = 24;
 variables['humidity'] = 40;
 
-// Id
-app.get('/id', function(req, res){
-  
-  var answer = new Object();
-  answer.id = pi.id;
-  answer.name  = pi.name;
-  answer.connected = true;
-  
-  res.json(answer);
-});
-
-// Variables
-app.get('/:variable', function(req, res){
-
-  var answer = new Object();
-
-  answer.id = pi.id;
-  answer.name  = pi.name;
-  answer.connected = true;
-
-  answer[req.params.variable] = variables[req.params.variable];
-
-  res.json(answer);
-});
-
-// Camera snapshot
-app.get('/camera/snapshot', function(req, res){
-
-  var answer = new Object();
-
-  camera.start();
-
-  camera.on("read", function(err, timestamp, filename){
-     console.log("Picture recorded");
-     camera.stop();
-  });
-
-  answer.id = pi.id;
-  answer.name  = pi.name;
-  answer.connected = true;
-  answer.message = 'Picture saved';
-  res.json(answer);
-
-});
-
-// Digital write
-app.get('/digital/:pin/:state', function(req, res){
-
-  var answer = new Object();
-
-  answer.id = pi.id;
-  answer.name  = pi.name;
-  answer.connected = true;
-
-  answer.message = 'Pin ' + req.params.pin + ' set to ' + req.params.state;
-
-  gpio.open(parseInt(req.params.pin), "output", function(err) {     
-    gpio.write(parseInt(req.params.pin), parseInt(req.params.state), function() {  
-      gpio.close(parseInt(req.params.pin));                   
-    });
-  });
-
-  // Send answer
-  res.json(answer);
-});
-
-// Digital read
-app.get('/digital/:pin', function(req, res){
-  
-  gpio.open(parseInt(req.params.pin), "input", function(err) {
-    gpio.read(parseInt(req.params.pin), function(err, value) {
-
+// Exports
+module.exports = function (app) {
+    
+    // Id
+    app.get('/id', function(req, res){
+      
       var answer = new Object();
       answer.id = pi.id;
       answer.name  = pi.name;
       answer.connected = true;
-      answer.return_value = value;
+      
+      res.json(answer);
+    });
+
+    // Variables
+    app.get('/:variable', function(req, res){
+
+      var answer = new Object();
+
+      answer.id = pi.id;
+      answer.name  = pi.name;
+      answer.connected = true;
+
+      answer[req.params.variable] = variables[req.params.variable];
+
+      res.json(answer);
+    });
+
+    // Camera snapshot
+    app.get('/camera/snapshot', function(req, res){
+
+      var answer = new Object();
+
+      camera.start();
+
+      camera.on("read", function(err, timestamp, filename){
+         console.log("Picture recorded");
+         camera.stop();
+      });
+
+      answer.id = pi.id;
+      answer.name  = pi.name;
+      answer.connected = true;
+      answer.message = 'Picture saved';
       res.json(answer);
 
-      gpio.close(parseInt(req.params.pin));  
     });
-  });
-  
-});
 
-var server = app.listen(80, function() {
-    console.log('Listening on port %d', server.address().port);
-});
+    // Digital write
+    app.get('/digital/:pin/:state', function(req, res){
+
+      var answer = new Object();
+
+      answer.id = pi.id;
+      answer.name  = pi.name;
+      answer.connected = true;
+
+      answer.message = 'Pin ' + req.params.pin + ' set to ' + req.params.state;
+
+      gpio.open(parseInt(req.params.pin), "output", function(err) {     
+        gpio.write(parseInt(req.params.pin), parseInt(req.params.state), function() {  
+          gpio.close(parseInt(req.params.pin));                   
+        });
+      });
+
+      // Send answer
+      res.json(answer);
+    });
+
+    // Digital read
+    app.get('/digital/:pin', function(req, res){
+      
+      gpio.open(parseInt(req.params.pin), "input", function(err) {
+        gpio.read(parseInt(req.params.pin), function(err, value) {
+
+          var answer = new Object();
+          answer.id = pi.id;
+          answer.name  = pi.name;
+          answer.connected = true;
+          answer.return_value = value;
+          res.json(answer);
+
+          gpio.close(parseInt(req.params.pin));  
+        });
+      });
+  
+  });
+};
 
 // Expose functions
 module.exports = {
